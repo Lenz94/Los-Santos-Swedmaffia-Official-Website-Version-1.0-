a3c9411c972906ee2da6a5c249894e1503069811
Enzo <d4rkleo94_enzo@hotmail.com>
2014-01-20 09:25:34 +0100

    Version 8.0

--- js/main.js
+++ js/main.js
@@ -15,4 +15,52 @@ $(document).ready(function () {
         audio.play();
         $(this).fadeOut();
     });
+    $("#icon2").mouseenter(function () {
+        audio.play();
+        $("#place2").fadeIn();
+    });
+    $("#iconplace2").mouseenter(function () {
+        audio.play();
+        $("#place2").fadeIn();
+    });
+    $("#place2").mouseleave(function () {
+        audio.play();
+        $(this).fadeOut();
+    });
+    $("#icon3").mouseenter(function () {
+        audio.play();
+        $("#place3").fadeIn();
+    });
+    $("#iconplace3").mouseenter(function () {
+        audio.play();
+        $("#place3").fadeIn();
+    });
+    $("#place3").mouseleave(function () {
+        audio.play();
+        $(this).fadeOut();
+    });
+    $("#icon4").mouseenter(function () {
+        audio.play();
+        $("#place4").fadeIn();
+    });
+    $("#iconplace4").mouseenter(function () {
+        audio.play();
+        $("#place4").fadeIn();
+    });
+    $("#place4").mouseleave(function () {
+        audio.play();
+        $(this).fadeOut();
+    });
+    $("#icon5").mouseenter(function () {
+        audio.play();
+        $("#place5").fadeIn();
+    });
+    $("#iconplace5").mouseenter(function () {
+        audio.play();
+        $("#place5").fadeIn();
+    });
+    $("#place5").mouseleave(function () {
+        audio.play();
+        $(this).fadeOut();
+    });
 });
